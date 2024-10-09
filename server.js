51b7c6a2dac143315f1156f105bd1018e190c4fa
saurabhdangi03 <104308074+saurabhdangi03@users.noreply.github.com>
2024-10-09 09:51:02 +0530

    commit

--- server.js
+++ server.js
@@ -1,60 +1,63 @@
-// const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
-// const mongoose = require('mongoose');
-// const typeDefs = require('./schema/bookingSchema');
-// const resolvers = require('./resolvers/bookingResolvers');
-// require('dotenv').config();
-
-// const startServer = async () => {
-//   const app = express();
-
-//   const server = new ApolloServer({ typeDefs, resolvers });
-//   await server.start();
-//   server.applyMiddleware({ app });
-
-//   // MongoDB connection
-//   mongoose.connect('mongodb+srv://saurabhdangi01:test123@cluster0.bsole.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-
-//   app.listen({ port: 4000 }, () =>
-//     console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
-//   );
-// };
-
-// startServer();
-
-
-const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
-const cors = require('cors');  // Add CORS support
-const { typeDefs, resolvers } = require('./schema/bookingSchema');
+const cors = require('cors');
+const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+const typeDefs = require('./schema/bookingSchema');
+const resolvers = require('./resolvers/bookingResolvers');
+require('dotenv').config();
 
-const app = express();
+const startServer = async () => {
+  const app = express();
 
-// Enable CORS
-app.use(cors());
+  app.use(cors());
 
-// MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
-
-async function startServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
   await server.start();
-  server.applyMiddleware({ app, path: '/api/graphql' });
-}
+  server.applyMiddleware({ app });
+
+  // MongoDB connection
+  mongoose.connect('mongodb+srv://saurabhdangi01:test123@cluster0.bsole.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
 
 startServer();
 
-module.exports = app;
+
+// const { ApolloServer } = require('apollo-server-express');
+// const express = require('express');
+// const cors = require('cors');  // Add CORS support
+// const { typeDefs, resolvers } = require('./schema/bookingSchema');
+// const mongoose = require('mongoose');
+// const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
+
+// const app = express();
+
+// // Enable CORS
+// app.use(cors());
+
+// // MongoDB connection
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+// });
+
+// async function startServer() {
+//   await server.start();
+//   server.applyMiddleware({ app, path: '/api/graphql' });
+// }
+
+// startServer();
+
+// module.exports = app;
